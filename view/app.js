f317ed46fa2798cc67956ed2b325a2db74e9ee9c
Gatsbyy <784007667@qq.com>
2017-11-23 09:50:47 +0800

    Update app.js
    del componentWillMount

--- view/app.js
+++ view/app.js
@@ -8,16 +8,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      count: 1,
     };
 
     this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentWillMount() {
-    this.setState({
-      count: this.state.count + 1,
-    });
   }
 
   handleClick(e) {
